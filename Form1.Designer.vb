0908782f7d789423dee9ffcc3af6341c1fee2097
DanielBenjaminFelix11 <152357649+DanielBenjaminFelix11@users.noreply.github.com>
2023-11-28 22:26:27 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(12, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(216, 32)
+        Label1.TabIndex = 0
+        Label1.Text = "Datos De Alumno"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(12, 50)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(291, 18)
+        Label2.TabIndex = 1
+        Label2.Text = "Nombre: Daniel Benjamin Gonzalez Felix"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(12, 77)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(72, 18)
+        Label3.TabIndex = 2
+        Label3.Text = "Edad: 16"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.Location = New Point(12, 106)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(233, 18)
+        Label4.TabIndex = 3
+        Label4.Text = "Direccion: Fidel Velazquez #316"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label5.Location = New Point(12, 137)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(164, 18)
+        Label5.TabIndex = 4
+        Label5.Text = "Telefono: 4428704491"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label6.Location = New Point(12, 167)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(309, 18)
+        Label6.TabIndex = 5
+        Label6.Text = "Correa: danielgonzalez11@cecyteq.edu.mx"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Programa1 DanielGonzalez"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
